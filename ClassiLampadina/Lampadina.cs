f14eb38ef2cacff92b5ae74b9760c9b83b41d0eb
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2020-10-23 19:16:53 +0200

    Aggiungere i file di progetto.

---
+++ ClassiLampadina/Lampadina.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassiLampadina
+{
+    public class Lampadina
+    {
+        public string Stato { get; set; }
+        public int Click_massimi { get; set; }
+        public int N_click { get; set; }
+
+        public Lampadina(string s, int c)
+        {
+            Stato = s;
+            Click_massimi = c;
+        }
+
+        public string Click()
+        {
+            N_click++;
+                if (N_click > Click_massimi)
+                {
+                    Stato = "ROTTA";
+                }
+                else if(Stato == "ACCESA")
+                {
+                    Stato = "SPENTA";
+                }
+                else if (Stato == "SPENTA")
+                {
+                    Stato = "ACCESA";
+                }
+            return Stato;
+        }
+        public string Descrizione()
+        {
+            return Stato;
+        }
+
+
+    }
+}
---
+++ TestLampadina/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassiLampadina;
+using System;
+
+namespace TestLampadina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int risposta;
+            Lampadina l = new Lampadina("SPENTA", 5);
+            Console.WriteLine("Per eseguire dei click digita = 1");
+            Console.WriteLine("Per controllare la lampadina digita = 2");
+            risposta = int.Parse(Console.ReadLine());
+            if (risposta == 1)
+            {
+                Console.WriteLine("Quanti click vuoi fare?");
+                int n = int.Parse(Console.ReadLine());
+                for (int j = 0; j < n; j++)
+                {
+                        Console.WriteLine("l1 = " + l.Click());
+                }
+            }else if (risposta == 2)
+            {
+                Console.WriteLine($"La lampadina è {l.Descrizione()}");
+            }
+        }
+    }
+}
